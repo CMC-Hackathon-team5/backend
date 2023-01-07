061358e25d6545975788fb19b75180d8d085ee9f
KCH <doolysmile1@naver.com>
2023-01-07 20:45:41 +0900

    feat : 초기 10개의 값 가져오기

--- src/main/java/com/cmc/selfdevelopment/domain/improvement/repository/ImprovementRepository.java
+++ src/main/java/com/cmc/selfdevelopment/domain/improvement/repository/ImprovementRepository.java
@@ -3,8 +3,11 @@ package com.cmc.selfdevelopment.domain.improvement.repository;
 import com.cmc.selfdevelopment.domain.improvement.entity.Improvement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ImprovementRepository extends JpaRepository<Improvement,Long> {
     Optional<Improvement> findByTitle(String title);
+
+    List<Improvement> findTop10ByOrderByIdAsc();
 }
