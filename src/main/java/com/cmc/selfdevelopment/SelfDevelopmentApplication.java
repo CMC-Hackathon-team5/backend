cd15b0175dd125928b3899a98a30eb6747306a42
KCH <doolysmile1@naver.com>
2023-01-07 15:00:56 +0900

    chore : second settiongs

--- src/main/java/com/cmc/selfdevelopment/SelfDevelopmentApplication.java
+++ src/main/java/com/cmc/selfdevelopment/SelfDevelopmentApplication.java
@@ -2,8 +2,10 @@ package com.cmc.selfdevelopment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SelfDevelopmentApplication {
 
 	public static void main(String[] args) {
