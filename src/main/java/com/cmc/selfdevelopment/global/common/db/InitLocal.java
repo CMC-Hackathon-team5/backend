b654ed3b9b32137846913ea087d50643bab1da8c
KCH <doolysmile1@naver.com>
2023-01-08 06:32:43 +0900

    test : test 추가

--- src/main/java/com/cmc/selfdevelopment/global/common/db/InitLocal.java
+++ src/main/java/com/cmc/selfdevelopment/global/common/db/InitLocal.java
@@ -1,5 +1,6 @@
 package com.cmc.selfdevelopment.global.common.db;
 
+import com.cmc.selfdevelopment.domain.improvement.dto.ChangeDoneDto;
 import com.cmc.selfdevelopment.domain.improvement.dto.CreateTodoDto;
 import com.cmc.selfdevelopment.domain.improvement.dto.ImprovementDto;
 import com.cmc.selfdevelopment.domain.improvement.service.ImprovementService;
@@ -59,18 +60,54 @@ public class InitLocal {
             SignUpRequestDto signUpRequestDto4 = new SignUpRequestDto("test4@test.com", "password", "테스트이름4");
             userService.userSignUp(signUpRequestDto4);
 
-            CreateTodoDto createTodoDto1 = new CreateTodoDto("운동", new Date(2023 - 1900, 1, 1));
             UserAccount userAccount1 = userService.findUserById(1L);
+            CreateTodoDto createTodoDto1 = new CreateTodoDto("운동", new Date(2023 - 1900, 1, 1));
             todoService.create(createTodoDto1, userAccount1);
             CreateTodoDto createTodoDto2 = new CreateTodoDto("독서", new Date(2023 - 1900, 1, 3));
-            UserAccount userAccount2 = userService.findUserById(1L);
-            todoService.create(createTodoDto2, userAccount2);
+            todoService.create(createTodoDto2, userAccount1);
             CreateTodoDto createTodoDto3 = new CreateTodoDto("피아노 연주", new Date(2023 - 1900, 1, 5));
-            UserAccount userAccount3 = userService.findUserById(1L);
-            todoService.create(createTodoDto3, userAccount3);
+            todoService.create(createTodoDto3, userAccount1);
             CreateTodoDto createTodoDto4 = new CreateTodoDto("알고리즘 공부", new Date(2023 - 1900, 1, 7));
-            UserAccount userAccount4 = userService.findUserById(1L);
-            todoService.create(createTodoDto4, userAccount4);
+            todoService.create(createTodoDto4, userAccount1);
+            CreateTodoDto createTodoDto5 = new CreateTodoDto("운동 열심히", new Date(2023 - 1900, 1, 2));
+            todoService.create(createTodoDto5, userAccount1);
+            CreateTodoDto createTodoDto6 = new CreateTodoDto("독서 열심히", new Date(2023 - 1900, 1, 2));
+            todoService.create(createTodoDto6, userAccount1);
+            CreateTodoDto createTodoDto7 = new CreateTodoDto("피아노 열심히", new Date(2023 - 1900, 1, 2));
+            todoService.create(createTodoDto7, userAccount1);
+            CreateTodoDto createTodoDto8 = new CreateTodoDto("알고리즘 열심히", new Date(2023 - 1900, 1, 2));
+            todoService.create(createTodoDto8, userAccount1);
+            CreateTodoDto createTodoDto9 = new CreateTodoDto("운동 꾸준히", new Date(2023 - 1900, 1, 3));
+            todoService.create(createTodoDto9, userAccount1);
+            CreateTodoDto createTodoDto10 = new CreateTodoDto("독서 꾸준히", new Date(2023 - 1900, 1, 3));
+            todoService.create(createTodoDto10, userAccount1);
+            CreateTodoDto createTodoDto11 = new CreateTodoDto("피아노 꾸준히", new Date(2023 - 1900, 1, 1));
+            todoService.create(createTodoDto11, userAccount1);
+            CreateTodoDto createTodoDto12 = new CreateTodoDto("알고리즘 꾸준히", new Date(2023 - 1900, 1, 1));
+            todoService.create(createTodoDto12, userAccount1);
+            CreateTodoDto createTodoDto13 = new CreateTodoDto("운동 매일", new Date(2023 - 1900, 1, 7));
+            todoService.create(createTodoDto13, userAccount1);
+            CreateTodoDto createTodoDto14 = new CreateTodoDto("독서 매일", new Date(2023 - 1900, 1, 7));
+            todoService.create(createTodoDto14, userAccount1);
+            CreateTodoDto createTodoDto15 = new CreateTodoDto("피아노 매일", new Date(2023 - 1900, 1, 7));
+            todoService.create(createTodoDto15, userAccount1);
+            CreateTodoDto createTodoDto16 = new CreateTodoDto("알고리즘 매일", new Date(2023 - 1900, 1, 6));
+            todoService.create(createTodoDto16, userAccount1);
+            CreateTodoDto createTodoDto17 = new CreateTodoDto("운동 하루씩", new Date(2023 - 1900, 1, 5));
+            todoService.create(createTodoDto17, userAccount1);
+            CreateTodoDto createTodoDto18 = new CreateTodoDto("독서 하루씩", new Date(2023 - 1900, 1, 4));
+            todoService.create(createTodoDto18, userAccount1);
+            CreateTodoDto createTodoDto19 = new CreateTodoDto("피아노 하루씩", new Date(2023 - 1900, 1, 3));
+            todoService.create(createTodoDto19, userAccount1);
+            CreateTodoDto createTodoDto20 = new CreateTodoDto("알고리즘 하루씩", new Date(2023 - 1900, 1, 8));
+            todoService.create(createTodoDto20, userAccount1);
+
+            todoService.changeCheck(userAccount1, new ChangeDoneDto("알고리즘 하루씩", new Date(2023 - 1900, 1, 8)));
+            todoService.changeCheck(userAccount1, new ChangeDoneDto("피아노 하루씩", new Date(2023 - 1900, 1, 3)));
+            todoService.changeCheck(userAccount1, new ChangeDoneDto("독서 하루씩", new Date(2023 - 1900, 1, 4)));
+            todoService.changeCheck(userAccount1, new ChangeDoneDto("운동 하루씩", new Date(2023 - 1900, 1, 5)));
+            todoService.changeCheck(userAccount1, new ChangeDoneDto("알고리즘 매일", new Date(2023 - 1900, 1, 6)));
+            todoService.changeCheck(userAccount1, new ChangeDoneDto("운동 매일", new Date(2023 - 1900, 1, 7)));
         };
     }
 
