f2793430da92483997333d270a3a8e37fc177710
KCH <doolysmile1@naver.com>
2023-01-07 14:39:33 +0900

    Initial Settings

---
+++ src/main/java/com/cmc/selfdevelopment/SelfDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.cmc.selfdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfDevelopmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmc/selfdevelopment/SelfDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmc.selfdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
